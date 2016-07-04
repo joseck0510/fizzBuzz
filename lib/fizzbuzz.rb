1dec8e66652d2e6ab723aec8656cf483ffa727d0
Jose <jcopoviking@outlook.com>
2016-07-04 22:38:57 +0100

    Commit 1

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,12 @@
 def fizzbuzz(number)
-  'Fizz'
+  number == rand(1..100)
+  if number % 3 == 0 && number % 5 != 0
+    'Fizz'
+  elsif number % 5 == 0 && number % 3 != 0
+    'Buzz'
+  elsif number % 5 == 0 && number % 3 == 0
+    'Fizzbuzz'
+  else
+    number
+  end
 end
